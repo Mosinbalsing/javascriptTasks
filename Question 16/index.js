31bc7f28c80589525ccddbfba5d6852de673d3fe
Mosin-M-B <mosinbalsing@gmail.com>
2024-02-01 16:11:30 +0530

    Student profile

--- Question 16/index.js
+++ Question 16/index.js
@@ -1,20 +1,32 @@
-var id = document.getElementById("id")
-var login = document.getElementById("login")
+var id = document.getElementById("id");
+var login = document.getElementById("login");
 var Name = document.getElementById("name");
-var img = document.getElementById("img")
+var img = document.getElementById("img");
 
-const url= "https://api.github.com/users/Mosin-M-B"
+const url = "https://api.github.com/users/Mosin-M-B";
 async function add() {
-    try {
-        const responce =await fetch(url)
-        const result = await responce.json();
-        console.log(result);
-        id.innerHTML=`<h1>ID :${result.id}</h1>`
-        login.innerHTML=`<h1>login :${result.login}</h1>`
-        Name.innerHTML=`<h1>Name :${result.name}</h1>`
-        img.innerHTML=`<img src="${result.avatar_url}"/>`
-    } catch (error) {
-        console.log(error);
-    }
-} 
-add()
+  try {
+    const responce = await fetch(url);
+    const result = await responce.json();
+    console.log(result);
+    id.innerHTML = `<h1>ID :${result.id}</h1>`;
+    login.innerHTML = `<h1>login :${result.login}</h1>`;
+    Name.innerHTML = `<h1>Name :${result.name}</h1>`;
+    img.innerHTML = `<img src="${result.avatar_url}"/>`;
+  } catch (error) {
+    console.log(error);
+  }
+}
+add();
+
+const students = [{
+    rollNo: 14,       
+    name: "Mosin Balsing",
+    age: 19,
+    gender: "Male",
+    favItCompany: "Wipro India",   
+    githubUrl: "https://api.github.com/users/Mosin-M-B",   
+    mailId: "mosinbalsing@gmail.com",
+    birthDate: "13-03-2004",
+    avatar :"https://avatars.githubusercontent.com/u/119176708?v=4"
+  }]
